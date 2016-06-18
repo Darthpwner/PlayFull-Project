6bfcb0b6104caa75478df0cf5233e820d46fd2ef
Darthpwner <matthewallenlin@gmail.com>
2016-06-18 16:39:35 -0700

    Made it more scalable

--- PlayFull Project/ViewController.swift
+++ PlayFull Project/ViewController.swift
@@ -19,65 +19,40 @@ class ViewController: UIViewController {
     
     @IBOutlet var parameterValue: UITextField!
     
-    @IBAction func getButton(sender: AnyObject) {
-        print("GET TEST")
-        
-        Alamofire.request(.GET, "http://jsonplaceholder.typicode.com/" + resource.text! + "/" + item.text!, parameters: [parameter.text!: parameterValue.text!])
+    func httpRequest(request: Alamofire.Method) {
+        Alamofire.request(request, "http://jsonplaceholder.typicode.com/" + resource.text! + "/" + item.text!, parameters: [parameter.text!: parameterValue.text!])
             .responseJSON { response in
                 
                 
                 if let JSON = response.result.value {
                     print("JSON: \(JSON)")
                 }
-                
+                    
                 else {
-                    print("GET request failure")
+                    print("Request failure")
                 }
                 
                 print("\n")
         }
     }
     
+    @IBAction func getButton(sender: AnyObject) {
+        print("GET TEST")
+        
+        httpRequest(Alamofire.Method.GET)
+    }
+    
     @IBAction func postButton(sender: AnyObject) {
         print("POST TEST")
         
-        let parameters = [
-            "foo": "bar",
-            "baz": ["a", 1],
-            "qux": [
-                "x": 1,
-                "y": 2,
-                "z": 3
-            ]
-        ]
-        
-        Alamofire.request(.POST, "http://jsonplaceholder.typicode.com/", parameters: parameters)
-            .validate()
-            .responseJSON { response in
-                switch response.result {
-                case .Success:
-                    print("Validation Successful for POST");
-                case .Failure(let error):
-                    print(error)
-                }
-        }
-        // HTTP body: foo=bar&baz[]=a&baz[]=1&qux[x]=1&qux[y]=2&qux[z]=3
+        httpRequest(Alamofire.Method.POST)
     }
     
     
     @IBAction func deleteButton(sender: AnyObject) {
         print("DELETE TEST")
         
-        Alamofire.request(.DELETE, "http://jsonplaceholder.typicode.com/")
-            .validate()
-            .responseJSON { response in
-                switch response.result {
-                case .Success:
-                    print("Validation Successful for DELETE");
-                case .Failure(let error):
-                    print(error)
-                }
-        }
+        httpRequest(Alamofire.Method.DELETE)
     }
 
     override func viewDidLoad() {
